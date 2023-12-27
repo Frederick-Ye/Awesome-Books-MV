d6f3e47bfd275ebba056be0ea540148b2100cc6c
Frederick Ye <yenainglinn7@gmail.com>
2023-12-28 01:55:56 +0630

    fix linter errors

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 let books = [];
-let bookSection = document.getElementById('bookList');
-let titleInput = document.getElementById('title');
-let authorInput = document.getElementById('author');
-let addBtn = document.getElementById('openBtn');
+const bookSection = document.getElementById('bookList');
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const addBtn = document.getElementById('openBtn');
+
+function renderBooks() {
+  bookSection.innerHTML = '';
+
+  books.forEach((book) => {
+    const bookHtml = `<div class="book">
+                        <p>${book.title}</p>
+                        <p>${book.author}</p>
+                        <button class="closeBtn">Remove</button>
+                      </div>`;
+    bookSection.innerHTML += bookHtml;
+  });
+}
+
+function saveBooks() {
+  localStorage.setItem('books', JSON.stringify(books));
+}
 
 window.addEventListener('load', () => {
   const storedBooks = localStorage.getItem('books');
@@ -33,7 +50,6 @@ function addBook() {
 
 function removeBook(event) {
   if (event.target.classList.contains('closeBtn')) {
-
     const bookContainer = event.target.closest('.book');
     const bookIndex = Array.from(bookSection.children).indexOf(bookContainer);
 
@@ -44,24 +60,6 @@ function removeBook(event) {
     saveBooks();
   }
 }
-
-function renderBooks() {
-  bookSection.innerHTML = '';
-
-  books.forEach(book => {
-    const bookHtml = `<div class="book">
-                        <p>${book.title}</p>
-                        <p>${book.author}</p>
-                        <button class="closeBtn">Remove</button>
-                      </div>`;
-    bookSection.innerHTML += bookHtml;
-  });
-}
-
-function saveBooks() {
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
 
 addBtn.addEventListener('click', addBook);
 bookSection.addEventListener('click', removeBook);
